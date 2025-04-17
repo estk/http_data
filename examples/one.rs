89554e3fd41b2ac7557d0556b1a65fbaa0a492c7
Evan Simmons <esims89@gmail.com>
2025-04-16 22:26:52 -0700

    rewrite

---
+++ examples/one.rs
@@ -0,0 +1,121 @@
+use enumflags2::BitFlags;
+use http_data::{DataKinds, HeaderData, Headers, Method, MethodData, RequestData};
+
+use std::collections::HashMap;
+
+pub struct ReqWrap<'m> {
+    method: &'m str,
+    headers: HashMap<String, String>,
+    _body: String,
+}
+impl ReqWrap<'_> {
+    pub fn set_header(&mut self, name: &str, value: &str) {
+        self.headers.insert(name.to_string(), value.to_string());
+    }
+}
+
+impl Default for ReqWrap<'_> {
+    fn default() -> Self {
+        ReqWrap {
+            method: "GET",
+            headers: HashMap::new(),
+            _body: String::new(),
+        }
+    }
+}
+
+impl RequestData for ReqWrap<'_> {
+    fn method_providers(&self) -> BitFlags<DataKinds> {
+        DataKinds::Str | DataKinds::Bytes
+    }
+    fn headers_providers(&self) -> BitFlags<DataKinds> {
+        DataKinds::Str | DataKinds::Bytes
+    }
+
+    fn provide_method(&self, dk: DataKinds) -> Option<MethodData> {
+        match dk {
+            DataKinds::Str => Some(MethodData::Str(self.method)),
+            DataKinds::Bytes => Some(MethodData::Bytes(self.method.as_bytes())),
+            _ => None,
+        }
+    }
+    fn provide_headers<'s>(&'s self, dk: DataKinds) -> Option<HeaderData<'s>> {
+        match dk {
+            DataKinds::Str => {
+                let iter = self.headers.iter().map(|(k, v)| (k.as_str(), v.as_str()));
+                Some(HeaderData::Str(Box::new(iter)))
+            }
+            DataKinds::Bytes => {
+                let iter = self
+                    .headers
+                    .iter()
+                    .map(|(k, v)| (k.as_bytes(), v.as_bytes()));
+                Some(HeaderData::Bytes(Box::new(iter)))
+            }
+            _ => None,
+        }
+    }
+}
+impl Headers<str, str> for ReqWrap<'_> {
+    fn headers<'s>(&'s self) -> impl Iterator<Item = (&'s str, &'s str)>
+    where
+        &'s str: 's,
+    {
+        self.headers.iter().map(|(k, v)| (k.as_str(), v.as_str()))
+    }
+}
+
+impl Headers<[u8], [u8]> for ReqWrap<'_> {
+    fn headers<'s>(&'s self) -> impl Iterator<Item = (&'s [u8], &'s [u8])>
+    where
+        &'s [u8]: 's,
+    {
+        self.headers
+            .iter()
+            .map(|(k, v)| (k.as_bytes(), v.as_bytes()))
+    }
+}
+
+impl Method<str> for ReqWrap<'_> {
+    fn method(&self) -> &str {
+        &self.method
+    }
+}
+impl Method<[u8]> for ReqWrap<'_> {
+    fn method(&self) -> &[u8] {
+        &self.method.as_bytes()
+    }
+}
+
+fn main() {
+    use http_data::{DataKinds, HeaderData, MethodData, RequestData as _};
+
+    let mut req = ReqWrap::default();
+    req.set_header("Content-Type", "application/json");
+
+    let method = if let Some(MethodData::Str(m)) = req.provide_method(DataKinds::Str) {
+        m
+    } else if let Some(MethodData::Bytes(m)) = req.provide_method(DataKinds::Bytes) {
+        std::str::from_utf8(m).unwrap()
+    } else {
+        panic!("Unsupported method");
+    };
+
+    dbg!(method);
+    let mut headers: Vec<(String, String)> = vec![];
+    if let Some(HeaderData::Str(hs)) = req.provide_headers(DataKinds::Str) {
+        for (name, value) in hs {
+            headers.push((name.to_string(), value.to_string()));
+        }
+    } else if let Some(HeaderData::Bytes(hs)) = req.provide_headers(DataKinds::Bytes) {
+        for (name, value) in hs {
+            let name = String::from_utf8_lossy(name).to_string();
+            let value = String::from_utf8_lossy(value).to_string();
+            headers.push((name, value));
+        }
+    } else {
+        panic!("Unsupported headers");
+    }
+
+    dbg!(headers);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,79 @@
-pub use bytes;
-use bytes::Buf;
-use http::{HeaderName, HeaderValue, Method, Uri};
-pub use http_body;
-use http_body_util::{BodyDataStream, Collected};
+use arrayvec::ArrayVec;
+use enumflags2::BitFlags;
 
-pub trait Response {
-    type Data: Buf;
-
-    fn status(&self) -> u16;
-    fn headers(&self) -> impl Iterator<Item = (&HeaderName, &HeaderValue)>;
-    fn body_collected(self) -> Collected<Self::Data>;
-    fn body_stream(self) -> BodyDataStream<Self::Data>;
+// should this have an emergent ordering or should it be configurable by the user/implementer
+#[enumflags2::bitflags]
+#[repr(u8)]
+#[derive(Copy, Clone, PartialEq)]
+pub enum DataKinds {
+    Bytes = 1 << 0,
+    Str = 1 << 1,
+    HttpParsed = 1 << 2,
+}
+impl DataKinds {
+    pub const fn count() -> usize {
+        <BitFlags<DataKinds>>::ALL.bits_c().count_ones() as usize
+    }
 }
 
-pub trait Request {
-    type Data: Buf;
+/// Allow the trait user to specify its data preferences
+pub struct DataKindPreference {
+    ordering: ArrayVec<DataKinds, { DataKinds::count() }>,
+}
 
-    fn method(&self) -> &Method;
-    fn uri(&self) -> &Uri;
-    fn headers(&self) -> impl Iterator<Item = (&HeaderName, &HeaderValue)>;
-    fn body_collected(self) -> Collected<Self::Data>;
-    fn body_stream(self) -> BodyDataStream<Self::Data>;
+impl DataKindPreference {
+    pub fn top(&self, provided: BitFlags<DataKinds>) -> Option<DataKinds> {
+        for &x in &self.ordering {
+            if provided.contains(x) {
+                return Some(x);
+            }
+        }
+
+        None
+    }
+}
+
+pub enum MethodData<'a> {
+    Str(&'a str),
+    Bytes(&'a [u8]),
+    Parsed(http::Method),
+}
+pub enum HeaderNameData<'a> {
+    Str(&'a str),
+    Bytes(&'a [u8]),
+    Parsed(http::HeaderName),
+}
+pub enum HeaderValueData<'a> {
+    Str(&'a str),
+    Bytes(&'a [u8]),
+    Parsed(http::HeaderValue),
+}
+pub enum HeaderData<'a> {
+    Str(Box<dyn Iterator<Item = (&'a str, &'a str)> + 'a>),
+    Bytes(Box<dyn Iterator<Item = (&'a [u8], &'a [u8])> + 'a>),
+    Parsed(Box<dyn Iterator<Item = (http::HeaderName, http::HeaderValue)> + 'a>),
+}
+
+pub trait RequestData {
+    fn method_providers(&self) -> BitFlags<DataKinds>;
+    fn headers_providers(&self) -> BitFlags<DataKinds>;
+
+    // I think these should be possible to auto-implement
+    fn provide_method<'s>(&'s self, dk: DataKinds) -> Option<MethodData<'s>>;
+    fn provide_headers<'s>(&'s self, dk: DataKinds) -> Option<HeaderData<'s>>;
+
+    fn provide_preferred_method(&self, prefs: DataKindPreference) -> Option<MethodData> {
+        let provided = self.method_providers();
+        prefs.top(provided).and_then(|dk| self.provide_method(dk))
+    }
+}
+pub trait Method<M: ?Sized> {
+    fn method(&self) -> &M;
+}
+
+pub trait Headers<Name: ?Sized, Value: ?Sized> {
+    fn headers<'s>(&'s self) -> impl Iterator<Item = (&'s Name, &'s Value)>
+    where
+        Name: 's,
+        Value: 's;
 }
